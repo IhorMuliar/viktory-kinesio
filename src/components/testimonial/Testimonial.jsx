74834445bbbd9c7dc84ecbdf7ea85eec6b69f728
IhorMuliar <goga440@gmail.com>
2024-09-19 21:32:09 +0300

    Update reviews

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,3 +1,4 @@
+import quote from '../../assets/images/quote.png';
 import styles from './Testimonial.module.css';
 
 const Testimonial = () => {
@@ -5,27 +6,15 @@ const Testimonial = () => {
     <section className={styles.testimonial}>
       <div className={styles.container}>
         <div className={styles.text_container}>
-          <h2 className="bold-xl">Title</h2>
-          <p>
-            Description - the main idea. Use 1-2 sentences in 1-3 lines if centered.
-            For more text, use a different type of content alignment.
-          </p>
+          <h2 className="bold-xl">Відгуки</h2>
         </div>
-        <div className={styles.reviews}>
-          <div className={styles.review}>
-            <p>
-              Customer review. Use 1-2 sentences in 2-4 lines. Ideally, the text in reviews should be of equal weight
-              (number of lines +- the same), so that some reviews do not seem heavy + long descriptions are read by a
-              small percentage of people
-            </p>
-          </div>
-          <div className={styles.review}>
-            <p>
-              Customer review. Use 1-2 sentences in 2-4 lines. Ideally, the text in reviews should be of equal weight
-              (number of lines +- the same), so that some reviews do not seem heavy + long descriptions are read by a
-              small percentage of people
-            </p>
-          </div>
+        <div className={styles.review}>
+          <img src={quote} alt="quote"/>
+          <p className="">
+            Customer review. Use 1-2 sentences in 2-4 lines. Ideally, the text in reviews should be of equal weight
+            (number of lines +- the same), so that some reviews do not seem heavy + long descriptions are read by a
+            small percentage of people
+          </p>
         </div>
       </div>
     </section>
