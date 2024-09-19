0fc4cdd35f87da02351dfc778bb295358ab16da4
IhorMuliar <goga440@gmail.com>
2024-09-19 21:18:04 +0300

    Update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,21 +1,10 @@
-// import logo from "../../assets/images/logo.png";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <footer>
       <div className={styles.container}>
-        {/*<img src={logo} alt="logo"/>*/}
-        <p>2024</p>
-        <a>
-          <img src="" alt="instagram"/>
-        </a>
-        <a>
-          <img src="" alt="instagram"/>
-        </a>
-        <a>
-          <img src="" alt="instagram"/>
-        </a>
+        <p className="regular-s">© 2024 Viktory Kinesio. All rights reserved.</p>
       </div>
     </footer>
   )
