6e666151307529013390d223c776499bfe4a79c2
IhorMuliar <goga440@gmail.com>
2024-09-22 00:37:26 +0300

    Update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,34 @@
 import styles from "./Footer.module.css";
+import logo from "../../assets/images/logo.svg";
 
 const Footer = () => {
   return (
     <footer>
       <div className={styles.container}>
-        <p className="regular-s">© 2024 Viktory Kinesio. All rights reserved.</p>
+        <ul className={styles.navigation_links}>
+          <li>
+            <a className="regular-s" href="#">Про мене</a>
+          </li>
+          <li>
+            <a className="regular-s" href="#">Спеціалізація</a>
+          </li>
+          <li>
+            <a className="regular-s" href="#">Питання та відповіді</a>
+          </li>
+          <li>
+            <a className="regular-s" href="#">Відгуки</a>
+          </li>
+          <li>
+            <a className="regular-s" href="#">Контакти</a>
+          </li>
+        </ul>
+        <a href="/" className={styles.logo}>
+          <img src={logo} alt="logo"/>
+        </a>
+        <div className={styles.description}>
+          <p className="regular-s">© 2024 Viktory Kinesio</p>
+          <p className="regular-s">All rights reserved</p>
+        </div>
       </div>
     </footer>
   )
