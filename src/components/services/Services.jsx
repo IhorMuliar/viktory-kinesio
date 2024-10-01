bc42ea8cadc0bd58d29dddb12996b793737909b5
IhorMuliar <goga440@gmail.com>
2024-10-01 15:59:42 +0300

    Update services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -2,113 +2,31 @@ import icon1 from "../../assets/images/icon1.png";
 import icon2 from "../../assets/images/icon2.png";
 import icon3 from "../../assets/images/icon3.png";
 import styles from "./Services.module.css";
+import {services} from "./constants.js";
+
+const icons = [icon1, icon2, icon3];
 
 const Services = () => {
   return (
     <section className={styles.services}>
       <div className={styles.container}>
-        <h2 className="bold-xl">
-          Терапевтичні послуги
-        </h2>
+        <h2 className="bold-xl">Терапевтичні послуги</h2>
         <ul>
-          <div className={styles.row}>
-            <li>
-              <h3 className="bold-m">
-                Фізіотерапевт
-              </h3>
-              <img src={icon1} alt="test icon" />
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut porttitor dui. Quisque volutpat felis
-                vel
-                est accumsan, non efficitur mi suscipit. In blandit nunc urna, a accumsan sapien mattis vel. Nam
-                interdum
-                lorem sed est suscipit, id euismod risus dictum. Nunc et convallis justo, et vehicula metus. Duis
-                pretium
-                quam non bibendum congue. Suspendisse a felis dolor.
-              </p>
-            </li>
-            <li>
-              <h3 className="bold-m">
-                Остеопат
-              </h3>
-              <img src={icon2} alt="test icon"/>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut porttitor dui. Quisque volutpat felis
-                vel
-                est accumsan, non efficitur mi suscipit. In blandit nunc urna, a accumsan sapien mattis vel. Nam
-                interdum
-                lorem sed est suscipit, id euismod risus dictum. Nunc et convallis justo, et vehicula metus. Duis
-                pretium
-                quam non bibendum congue. Suspendisse a felis dolor.
-              </p>
-            </li>
-            <li>
-              <h3 className="bold-m">
-                Енерготерапевт
-              </h3>
-              <img src={icon3} alt="test icon"/>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut porttitor dui. Quisque volutpat felis
-                vel
-                est accumsan, non efficitur mi suscipit. In blandit nunc urna, a accumsan sapien mattis vel. Nam
-                interdum
-                lorem sed est suscipit, id euismod risus dictum. Nunc et convallis justo, et vehicula metus. Duis
-                pretium
-                quam non bibendum congue. Suspendisse a felis dolor.
-              </p>
-            </li>
-          </div>
-          <div className={styles.row}>
-            <li>
-              <h3 className="bold-m">
-                Фізіотерапевт
-              </h3>
-              <img src={icon1} alt="test icon" />
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut porttitor dui. Quisque volutpat felis
-                vel
-                est accumsan, non efficitur mi suscipit. In blandit nunc urna, a accumsan sapien mattis vel. Nam
-                interdum
-                lorem sed est suscipit, id euismod risus dictum. Nunc et convallis justo, et vehicula metus. Duis
-                pretium
-                quam non bibendum congue. Suspendisse a felis dolor.
-              </p>
-            </li>
-            <li>
-              <h3 className="bold-m">
-                Остеопат
-              </h3>
-              <img src={icon2} alt="test icon"/>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut porttitor dui. Quisque volutpat felis
-                vel
-                est accumsan, non efficitur mi suscipit. In blandit nunc urna, a accumsan sapien mattis vel. Nam
-                interdum
-                lorem sed est suscipit, id euismod risus dictum. Nunc et convallis justo, et vehicula metus. Duis
-                pretium
-                quam non bibendum congue. Suspendisse a felis dolor.
-              </p>
-            </li>
-            <li>
-              <h3 className="bold-m">
-                Енерготерапевт
-              </h3>
-              <img src={icon3} alt="test icon"/>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut porttitor dui. Quisque volutpat felis
-                vel
-                est accumsan, non efficitur mi suscipit. In blandit nunc urna, a accumsan sapien mattis vel. Nam
-                interdum
-                lorem sed est suscipit, id euismod risus dictum. Nunc et convallis justo, et vehicula metus. Duis
-                pretium
-                quam non bibendum congue. Suspendisse a felis dolor.
-              </p>
-            </li>
-          </div>
+          {services.map((service, index) => (
+            <div className={styles.row} key={index}>
+              {services.slice(index * 3, index * 3 + 3).map((item, i) => (
+                <li key={i}>
+                  <h3 className="bold-m">{item.title}</h3>
+                  <img src={icons[i % icons.length]} alt="service icon"/>
+                  <p>{item.description}</p>
+                </li>
+              ))}
+            </div>
+          ))}
         </ul>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Services;
