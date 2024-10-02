6c7ba9a3773391d3d05dffe812a69eb17be4dde2
IhorMuliar <goga440@gmail.com>
2024-10-02 18:33:49 +0300

    Update services section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,40 +1,22 @@
-import icon1 from "../../assets/images/icon1.png";
-import icon2 from "../../assets/images/icon2.png";
-import icon3 from "../../assets/images/icon3.png";
+import icon from "../../assets/images/test-icon.svg";
 import styles from "./Services.module.css";
-import { services } from "./constants.js";
-
-const icons = [icon1, icon2, icon3];
-
-const chunkArray = (arr, chunkSize) => {
-  const chunks = [];
-  for (let i = 0; i < arr.length; i += chunkSize) {
-    chunks.push(arr.slice(i, i + chunkSize));
-  }
-  return chunks;
-};
+import {services} from "./constants.js";
 
 const Services = () => {
-  const groupedServices = chunkArray(services, 3);
-
   return (
     <section className={styles.services} id="services">
-      <div className={styles.container}>
-        <h2 className="bold-xl">Терапевтичні послуги</h2>
-        <ul>
-          {groupedServices.map((group, groupIndex) => (
-            <div className={styles.row} key={groupIndex}>
-              {group.map((item, i) => (
-                <li key={i}>
-                  <h3 className="bold-m">{item.title}</h3>
-                  <img src={icons[i % icons.length]} alt="service icon" />
-                  <p>{item.description}</p>
-                </li>
-              ))}
+      <h2 className="bold-xl">Терапевтичні послуги</h2>
+      <ul>
+        {services.map((service, index) => (
+          <li key={index}>
+            <div className={styles.icon}>
+              <img src={icon} alt="service icon"/>
             </div>
-          ))}
-        </ul>
-      </div>
+            <h3 className="bold-m">{service.title}</h3>
+            <p>{service.description}</p>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 };
