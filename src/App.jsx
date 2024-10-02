827916d8437b8e958237c7d21bd3a3cab99a5e4b
IhorMuliar <goga440@gmail.com>
2024-10-02 13:49:58 +0300

    Added process 2 section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Content from './components/content/Content';
 import Services from './components/services/Services';
 import Testimonial from './components/testimonial/Testimonial';
 import Process from './components/process/Process';
+import Process2 from './components/process2/Process2';
 import Faq from './components/faq/Faq';
 import Footer from './components/footer/Footer';
 import Contacts from "./components/contacts/Contacts";
@@ -16,6 +17,7 @@ function App() {
       <Content />
       <Services />
       <Process />
+      <Process2 />
       <Faq />
       <Testimonial />
       <Contacts />
---
+++ src/components/process2/process2.jsx
@@ -0,0 +1,27 @@
+import styles from "./Process2.module.css";
+import liquid from "../../assets/images/liquid.mp4";
+import image from "../../assets/images/hero-portrait.jpg";
+import {processes} from "./constants.js";
+
+const Process2 = () => {
+  return (
+    <section className={styles.process2}>
+      <div className={styles.video}>
+        <video src={liquid} autoPlay={true} loop={true} muted={true} playsInline={true} preload="auto" />
+      </div>
+      <div className={styles.container}>
+        <img src={image} alt="test image"/>
+        <ul>
+          {processes.map((process) => (
+            <li>
+              <p className={styles.index}>/{process.index}</p>
+              <p className={styles.title}>{process.title}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+export default Process2;
