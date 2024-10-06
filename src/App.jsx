9f058cc90329e8fae2eb1a14b6e8049224909c43
IhorMuliar <goga440@gmail.com>
2024-10-06 15:25:48 +0300

    Minor refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from './components/hero/Hero';
 import Content from './components/content/Content';
 import Services from './components/services/Services';
 import Testimonial from './components/testimonial/Testimonial';
-import Process from './components/process2/process.jsx';
+import Process from './components/process/process.jsx';
 import Faq from './components/faq/Faq';
 import Footer from './components/footer/Footer';
 import Contacts from "./components/contacts/Contacts";
