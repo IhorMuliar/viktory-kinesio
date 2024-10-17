dea48b4ae1ba66daa28b6badcac0a70053716d8c
IhorMuliar <goga440@gmail.com>
2024-10-17 18:47:52 +0300

    Update header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,46 @@
+// import logo from '../../assets/images/logo.svg';
+// import styles from './Header.module.css';
+//
+// const Header = () => {
+//   return (
+//     <header>
+//       <div className={styles.container}>
+//         <a href="/" className={styles.logo}>
+//           <img src={logo} alt="logo"/>
+//         </a>
+//         <nav>
+//           <ul className={styles.navigation_links}>
+//             <li>
+//               <a className="regular-s" href="#content">Про мене</a>
+//             </li>
+//             <li>
+//               <a className="regular-s" href="#services">Спеціалізація</a>
+//             </li>
+//             <li>
+//               <a className="regular-s" href="#process">Процес</a>
+//             </li>
+//             <li>
+//               <a className="regular-s" href="#faq">Питання та відповіді</a>
+//             </li>
+//             <li>
+//               <a className="regular-s" href="#testimonial">Відгуки</a>
+//             </li>
+//             <li>
+//               <button className="primary size-l">
+//                 <span className="text">
+//                   Запис
+//                 </span>
+//               </button>
+//             </li>
+//           </ul>
+//         </nav>
+//       </div>
+//     </header>
+//   )
+// }
+//
+// export default Header;
+
 import logo from '../../assets/images/logo.svg';
 import styles from './Header.module.css';
 
@@ -5,9 +48,6 @@ const Header = () => {
   return (
     <header>
       <div className={styles.container}>
-        <a href="/" className={styles.logo}>
-          <img src={logo} alt="logo"/>
-        </a>
         <nav>
           <ul className={styles.navigation_links}>
             <li>
@@ -19,6 +59,9 @@ const Header = () => {
             <li>
               <a className="regular-s" href="#process">Процес</a>
             </li>
+            <a href="/" className={styles.logo}>
+              <img src={logo} alt="logo"/>
+            </a>
             <li>
               <a className="regular-s" href="#faq">Питання та відповіді</a>
             </li>
@@ -26,11 +69,7 @@ const Header = () => {
               <a className="regular-s" href="#testimonial">Відгуки</a>
             </li>
             <li>
-              <button className="secondary size-l">
-                <span className="text">
-                  Запис
-                </span>
-              </button>
+              <a className="regular-s" href="#testimonial">Контакти</a>
             </li>
           </ul>
         </nav>
